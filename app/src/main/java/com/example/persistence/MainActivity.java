0935b049770fbf36d19c9fc477983ab95c609d63
a19jimsa <a19jimsa@student.his.se>
2021-04-15 17:19:56 +0200

    Added Database classes. Added Mountain class.

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase database;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create
+        databaseHelper = new DatabaseHelper(this);
+        database = databaseHelper.getWritableDatabase();
+    }
+    private long addMountain(String name, int height) {
+        ContentValues values = new ContentValues();
+        values.put(DatabaseTables.Mountain.COLUMN_NAME_NAME, name);
+        values.put(DatabaseTables.Mountain.COLUMN_NAME_HEIGHT, height);
+        return database.insert(DatabaseTables.Mountain.TABLE_NAME, null, values);
+    }
+    private List<Mountain> getMountains() {
+        Cursor cursor = database.query(DatabaseTables.Mountain.TABLE_NAME, null, null, null, null, null, null);
+        List<Mountain> mountains = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            Mountain mountain = new Mountain(
+                    cursor.getLong(cursor.getColumnIndexOrThrow(DatabaseTables.Mountain.COLUMN_NAME_ID)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Mountain.COLUMN_NAME_NAME)),
+                    cursor.getInt(cursor.getColumnIndexOrThrow(DatabaseTables.Mountain.COLUMN_NAME_HEIGHT))
+            );
+            mountains.add(mountain);
+        }
+        cursor.close();
+        return mountains;
     }
 }
